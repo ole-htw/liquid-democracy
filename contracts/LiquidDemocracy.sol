ae0be4ec3acad3a545bcf094d6c889e334182f4c
Marcel Gregoriadis <marcel.gregoriadis@hu-berlin.de>
2022-07-10 19:51:06 +0200

    finished contract

--- contracts/LiquidDemocracy.sol
+++ contracts/LiquidDemocracy.sol
@@ -18,8 +18,10 @@ struct Proposal {
 
 contract LiquidDemocracy {
     Proposal[] proposals;
-    mapping(uint => mapping(address => Vote)) submittedVotes;
-    mapping(address => mapping(Domain => address)) delegations;
+    mapping(uint => Vote[]) proposalVotes; // proposalID => votes
+    mapping(uint => mapping(address => Vote)) submittedVotes; // proposalID => voter => value
+    mapping(address => mapping(Domain => address)) delegations; // who delegates what to who
+    mapping(address => mapping(Domain => address[])) delegators;  // who was delegated for what by whom
 
     function propose(Domain domain, string memory description) public payable {
         proposals.push(Proposal(
@@ -31,14 +33,39 @@ contract LiquidDemocracy {
 
     function vote(uint proposalID, Vote value) public payable {
         require(proposalID < proposals.length, "Proposal ID does not exist.");
+        Domain domain = proposals[proposalID].domain;
         require(
-            delegations[msg.sender][proposals[proposalID].domain] == address(0), 
+            delegations[msg.sender][domain] == address(0), 
             "You have delegated votes in this domain."
         );
+
         submittedVotes[proposalID][msg.sender] = value;
+        proposalVotes[proposalID].push(value);
+
+        // add delegated votes as well
+        for (uint i = 0; i < delegators[msg.sender][domain].length; i++) {
+            address voter = delegators[msg.sender][domain][i];
+            submittedVotes[proposalID][voter] = value;
+            proposalVotes[proposalID].push(value);
+        }
     }
 
     function delegate(Domain domain, address to) public payable {
         delegations[msg.sender][domain] = to;
+        delegators[to][domain].push(msg.sender);
+    }
+
+    function status(uint proposalID) public payable returns(int) {
+        int result = 0;
+        Vote[] memory votes = proposalVotes[proposalID];
+        for (uint i = 0; i < votes.length; i++) {
+            if (votes[i] == Vote.FOR) {
+                result++;
+            }
+            if (votes[i] == Vote.AGAINST) {
+                result--;
+            }
+        }
+        return result;
     }
 }
